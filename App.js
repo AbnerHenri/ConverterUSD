d18f607048950e7b5894f9ef1ccd84cbbb17f241
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-12-09 09:30:21 -0300

    Styled View and Title

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Title, Page, View } from './Styles/AppStyle'
 
 const App = ()  => {
+
+
   return (
   <Page>
     <Title>Welcome</Title>
+
     <View>
 
     </View>
--- Styles/AppStyle.js
+++ Styles/AppStyle.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components/native';
 
+const Background = "#6e47e0"
+
 export const Page = styled.SafeAreaView`
   flex : 1;
 
+  background-color: ${Background};
   justify-content: center;
   align-items: center;
 `
@@ -10,10 +13,12 @@ export const Page = styled.SafeAreaView`
 export const View = styled.View`
   width: 90%;
   height :60%;
-  background-color: blue;
 `
 
 export const Title = styled.Text`
-  font-size: 35px;
-  color : purple;
+  font-size: 45px;
+  color : white;
+  font-weight: 700;
+  letter-spacing: 2px;
+  transform: translateY(-35px);
 `
